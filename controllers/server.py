21b416fa7d7853f1aff0c5982c7942581bf83794
robnanola <robnanola@gmail.com>
2014-10-13 10:32:25 +0800

    used ajax to submit form async

--- controllers/server.py
+++ controllers/server.py
@@ -3,16 +3,38 @@ import urllib
 from google.appengine.api import users
 from google.appengine.ext import ndb
 import webapp2
+import json
+import datetime
 
 from conf import settings
 from models import core as core_model
+
+
+class jsonEncoder(json.JSONEncoder):
+    """
+    Converts ndb, datetime object to json.
+    """
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.isoformat()
+
+        elif isinstance(obj, ndb.Model):
+            return obj.to_dict()
+
+        elif isinstance(obj, users.User):
+            return obj.email()
+
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 # We set a parent key on the 'Greetings' to ensure that they are all in the same
 # entity group. Queries across the single entity group will be consistent.
 # However, the write rate should be limited to ~1/second.
 
 def guestbook_key(guestbook_name=settings.DEFAULT_GUESTBOOK_NAME):
-    """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
+    """
+    Constructs a Datastore key for a Guestbook entity with guestbook_name.
+    """
     return ndb.Key('Guestbook', guestbook_name)
 
 class MainPage(webapp2.RequestHandler):
@@ -21,8 +43,11 @@ class MainPage(webapp2.RequestHandler):
         guestbook_name = self.request.get('guestbook_name',
                                           settings.DEFAULT_GUESTBOOK_NAME)
         greetings_query = core_model.Greeting.query(
-            ancestor=guestbook_key(guestbook_name)).order(core_model.Greeting.date)
+            ancestor=guestbook_key(guestbook_name)).order(-core_model.Greeting.date)
         greetings = greetings_query.fetch(10)
+
+        greetings.reverse()
+
 
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
@@ -45,10 +70,6 @@ class MainPage(webapp2.RequestHandler):
 class Guestbook(webapp2.RequestHandler):
 
     def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each Greeting
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
-        # should be limited to ~1/second.
         guestbook_name = self.request.get('guestbook_name',
                                           settings.DEFAULT_GUESTBOOK_NAME)
         greeting = core_model.Greeting(parent=guestbook_key(guestbook_name))
@@ -59,5 +80,4 @@ class Guestbook(webapp2.RequestHandler):
         greeting.content = self.request.get('content')
         greeting.put()
 
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
+        self.response.out.write(json.dumps(greeting, cls=jsonEncoder))

9261e2e198b1466181417fe94c449537c6d62aae
robnanola <robnanola@gmail.com>
2014-10-13 17:12:00 +0800

    added rating field to model

--- models/core.py
+++ models/core.py
@@ -5,4 +5,5 @@ class Greeting(ndb.Model):
     """Models an individual Guestbook entry with author, content, and date."""
     author = ndb.UserProperty()
     content = ndb.StringProperty(indexed=False)
+    rating = ndb.IntegerProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)

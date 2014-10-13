d484b2e1f405c6223d2ff66fdef0eee0a16a54c5
robnanola <robnanola@gmail.com>
2014-10-13 14:22:49 +0800

    added updated greeting functionality. fix minor bugs

--- controllers/server.py
+++ controllers/server.py
@@ -12,14 +12,19 @@ from models import core as core_model
 
 class jsonEncoder(json.JSONEncoder):
     """
-    Converts ndb, datetime object to json.
+    Encodes ndb, datetime objects to json.
     """
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return obj.isoformat()
 
         elif isinstance(obj, ndb.Model):
-            return obj.to_dict()
+            obj_dct = obj.to_dict()
+            
+            #add id to the return dict 
+            obj_dct['id'] = obj.key.id()
+
+            return obj_dct
 
         elif isinstance(obj, users.User):
             return obj.email()
@@ -27,9 +32,6 @@ class jsonEncoder(json.JSONEncoder):
         else:
             return json.JSONEncoder.default(self, obj)
 
-# We set a parent key on the 'Greetings' to ensure that they are all in the same
-# entity group. Queries across the single entity group will be consistent.
-# However, the write rate should be limited to ~1/second.
 
 def guestbook_key(guestbook_name=settings.DEFAULT_GUESTBOOK_NAME):
     """
@@ -37,16 +39,41 @@ def guestbook_key(guestbook_name=settings.DEFAULT_GUESTBOOK_NAME):
     """
     return ndb.Key('Guestbook', guestbook_name)
 
-class MainPage(webapp2.RequestHandler):
+
+class BaseHandler(webapp2.RequestHandler):
+    """
+    User aware handler
+    """
+
+    @webapp2.cached_property
+    def user(self):
+        return users.get_current_user()
+
+    def render_template(self, template_name, context_data=None):
+        if not context_data:
+            context_data = {}
+
+        context_data['user'] = self.user
+
+        template = settings.JINJA_ENVIRONMENT.get_template(template_name)
+        self.response.write(template.render(context_data))
+
+    def json_response(self, data):
+        #add data
+        json_data = json.dumps(data, cls=jsonEncoder)
+        self.response.out.write(json_data)
+
+
+
+class MainPage(BaseHandler):
 
     def get(self):
         guestbook_name = self.request.get('guestbook_name',
                                           settings.DEFAULT_GUESTBOOK_NAME)
         greetings_query = core_model.Greeting.query(
             ancestor=guestbook_key(guestbook_name)).order(-core_model.Greeting.date)
+        
         greetings = greetings_query.fetch(10)
-
-        greetings.reverse()
 
 
         if users.get_current_user():
@@ -63,21 +90,35 @@ class MainPage(webapp2.RequestHandler):
             'url_linktext': url_linktext,
         }
 
-        template = settings.JINJA_ENVIRONMENT.get_template('/index.html')
-        self.response.write(template.render(template_values))
+        self.render_template('/index.html', context_data=template_values)
 
 
-class Guestbook(webapp2.RequestHandler):
+class Guestbook(BaseHandler):
 
     def post(self):
         guestbook_name = self.request.get('guestbook_name',
                                           settings.DEFAULT_GUESTBOOK_NAME)
-        greeting = core_model.Greeting(parent=guestbook_key(guestbook_name))
 
-        if users.get_current_user():
-            greeting.author = users.get_current_user()
+        if self.request.get('content'):
+            # create or update ??        
+            if self.request.get('id'):
+                greeting = core_model.Greeting.get_by_id(int(self.request.get('id')),
+                parent=guestbook_key(guestbook_name))
 
-        greeting.content = self.request.get('content')
-        greeting.put()
+                # check if we are updating greetings of the same user
+                if greeting.author != self.user:
+                    
+                    self.abort(404)
 
-        self.response.out.write(json.dumps(greeting, cls=jsonEncoder))
+            else:
+                greeting = core_model.Greeting(parent=guestbook_key(guestbook_name))
+
+                if self.user:
+                    greeting.author = self.user
+            greeting.content = self.request.get('content')
+            greeting.put()
+
+            self.json_response(greeting)
+
+        else:
+            self.json_response({})

edd2c67dc3e9668d1d44d71720589c41ff873f0c
robnanola <robnanola@gmail.com>
2014-10-14 20:50:41 +0800

    updated app-id

--- conf/settings.py
+++ conf/settings.py
@@ -1,11 +1,16 @@
 import os 
 import jinja2
+from filters import slugify
+from filters import unslugify
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'../views')),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+JINJA_ENVIRONMENT.filters['slugify'] = slugify
+JINJA_ENVIRONMENT.filters['unslugify'] = unslugify
+
 DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
 
-APP_ID = 'your-app-id'
+APP_ID = 'guestbook-test-733'

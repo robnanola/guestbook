73029a442854460c5d1ea539e3888425ff88e40e
robnanola <robnanola@gmail.com>
2014-10-14 20:52:02 +0800

    added Hotels model, added Hotel reference to Greeting model

--- models/core.py
+++ models/core.py
@@ -4,6 +4,18 @@ from google.appengine.ext import ndb
 class Greeting(ndb.Model):
     """Models an individual Guestbook entry with author, content, and date."""
     author = ndb.UserProperty()
+    hotel = ndb.KeyProperty(kind='Hotel', indexed=True)
     content = ndb.StringProperty(indexed=False)
     rating = ndb.IntegerProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+
+class Hotel(ndb.Model):
+	"""
+	Model that holds hotel info
+	"""
+	name = ndb.StringProperty()
+	address1 = ndb.StringProperty()
+	address2 = ndb.StringProperty()
+	contact_nos = ndb.StringProperty()

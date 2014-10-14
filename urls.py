ff4c7fc038b07a8914fb3681fd36ef7c5622ae7c
robnanola <robnanola@gmail.com>
2014-10-15 01:18:43 +0800

    added routing to the new controllers

--- urls.py
+++ urls.py
@@ -5,10 +5,15 @@
 """
 
 from controllers import server
+from controllers import admin
 
 
 ROUTING = [
     ('/', server.MainPageHandler),
     ('/sign', server.GuestbookHandler),
     ('/user/([^/]+)/comments', server.UserCommentsHandler),
+    ('/hotel/([^/]+)', server.HotelCommentsHandler),
+    ('/hotel/([^/]+)/sign', server.HotelGuestbookHandler),
+    ('/admin/hotels', admin.HotelAdminHandler),
+    ('/admin/upload', admin.UploadHotelHandler),
 ]
